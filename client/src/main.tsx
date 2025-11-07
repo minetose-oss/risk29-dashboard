8afe051f4f85954f531fbfa71ada4c0e1d66824b
minetose-oss <minetose@gmail.com>
2025-11-08 00:28:49 +0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { registerServiceWorker } from "./lib/pwaUtils";
+import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router hook={useHashLocation}>
+    <App />
+  </Router>
+);
 
 // Register service worker for PWA
 if (import.meta.env.PROD) {
